255f0ba965749ba92692b11f8b534c63d85a3349
zeesann <panatchakorn.s@ku.th>
2026-09-17 17:23:53 +0700

    Add files via upload

--- Pair.java
+++ Pair.java
@@ -1,10 +1,25 @@
 /**
  * TODO: ทำให้คลาสนี้เป็น Generic <K, V> ที่สามารถเก็บอ็อบเจกต์ได้ 2 ชนิด
  */
-public class Pair {
+public class Pair <K,V> {
     // TODO: สร้างฟิลด์ private final สำหรับ key และ value
-    
+    private final K key;
+    private final V value;
     // TODO: สร้าง Constructor ที่รับ key และ value
+    
+    public Pair (K key, V value){
+        this.key = key;
+        this.value = value;
+
+    }
 
     // TODO: สร้าง Getters สำหรับ key และ value
+
+    public K getKey(){
+        return  key;
+    }
+
+    public V getValue(){
+        return  value;
+    }
 }
--- PairTest.java
+++ PairTest.java
@@ -3,14 +3,18 @@ public class PairTest {
         System.out.println("--- Testing Pair Class ---");
         
         // TODO: สร้าง Pair<String, Integer> และทดลองใช้งาน
-        // Pair<String, Integer> wordCount = new Pair<>("hello", 5);
-        // System.out.println("Key: " + wordCount.getKey());   // ควรจะได้ "hello"
-        // System.out.println("Value: " + wordCount.getValue()); // ควรจะได้ 5
+         Pair<String, Integer> wordCount = new Pair<>("hello", 5);
+         System.out.println("Key: " + wordCount.getKey());   // ควรจะได้ "hello"
+         System.out.println("Value: " + wordCount.getValue()); // ควรจะได้ 5
+
+        
 
         // TODO: สร้าง Pair<Product, Boolean> และทดลองใช้งาน
-        // Product apple = new Product("p1", "Apple", "Fruit", 10.0, 50);
-        // Pair<Product, Boolean> productStatus = new Pair<>(apple, true);
-        // System.out.println("Product: " + productStatus.getKey().name()); // ควรจะได้ "Apple"
-        // System.out.println("Is Available: " + productStatus.getValue()); // ควรจะได้ true
+         Product apple = new Product("p1", "Apple", "Fruit", 10.0, 50);
+         Pair<Product, Boolean> productStatus = new Pair<>(apple, true);
+         System.out.println("Product: " + productStatus.getKey().name()); // ควรจะได้ "Apple"
+         System.out.println("Is Available: " + productStatus.getValue()); // ควรจะได้ true
+
+        
     }
 }
